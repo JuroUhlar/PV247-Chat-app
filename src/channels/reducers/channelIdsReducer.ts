f38bd680824c9cccc09e4f0979d3b14c191e15dc
Juraj Uhlar <JurajU@kentico.com>
2018-12-02 17:45:03 +0100

    Fix channelIds reducer

--- src/channels/reducers/channelIdsReducer.ts
+++ src/channels/reducers/channelIdsReducer.ts
@@ -1,6 +1,7 @@
 import * as Immutable from 'immutable';
-import { CHANNEL_CREATE, CHANNEL_DELETE } from '../../shared/constants/actionTypes';
+import { CHANNEL_CREATE, CHANNEL_DELETE, CHANNELS_FETCH__SUCCESS, CHANNELS_POST__SUCCESS } from '../../shared/constants/actionTypes';
 import { getInitialChannels } from '../utils/getInitialChannels';
+import { IChannelServerModel } from '../models/Channel';
 
 
 const initialState = getInitialChannels().keySeq().toOrderedSet();
@@ -8,6 +9,25 @@ const initialState = getInitialChannels().keySeq().toOrderedSet();
 export const channelIdsReducer = (prevState: Immutable.OrderedSet<Uuid> = initialState, action: Action):
   Immutable.OrderedSet<Uuid> => {
   switch (action.type) {
+
+    case CHANNELS_FETCH__SUCCESS: {
+      const receivedObjects = action.payload.channels;
+      const channelIdsInCorrectOrder = receivedObjects
+        .map((value: IChannelServerModel) => value && value.id)
+        .reverse();
+
+      return Immutable.OrderedSet<Uuid>(channelIdsInCorrectOrder);
+    }
+
+    case CHANNELS_POST__SUCCESS: {
+      const channel: IChannelServerModel = action.payload.channel;
+      const oldId = channel.customData.clientId;
+      const serverId = channel.id;
+
+      return prevState
+        .map((id: Uuid) => (id === oldId) ?  serverId : id)
+        .toOrderedSet();
+    }
 
     case CHANNEL_CREATE: {
       return prevState.add(action.payload.id);
