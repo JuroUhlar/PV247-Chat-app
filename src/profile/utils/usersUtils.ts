e119c9c5b24343d12606400b1c98328ff3586fae
LindaH32 <lindah@kentico.com>
2018-12-04 20:19:53 +0100

    Get rid off unused method getInitialUSers

--- src/profile/utils/usersUtils.ts
+++ src/profile/utils/usersUtils.ts
@@ -1,6 +1,4 @@
-import * as Immutable from 'immutable';
 import * as memoizee from 'memoizee';
-import { IUser, User } from '../models/User';
 import { IUsersInfo } from '../models/IUsersInfo';
 
 export const harryId = '9bf3232e-01a6-4370-9110-c57bc5233190';
@@ -13,31 +11,5 @@ const getCurrentUser = (state: IUsersInfo) => {
 
   return currentUser;
 };
-
 const memoizedGetCurrentUser = memoizee(getCurrentUser);
 export { memoizedGetCurrentUser as getCurrentUser };
-
-export const getJane = (): IUser => (
-  new User({
-    id: janeId,
-    email: 'jane@gmail.com',
-    name: 'JaneD',
-    avatarPath: 'https://i.imgur.com/SElyTHD.jpg',
-  })
-);
-
-export const getInitialUsers = (): Immutable.Map<Uuid, IUser> => Immutable.Map({
-  [harryId]: new User({
-    id: harryId,
-    email: 'harry@gmail.com',
-    name: 'HarryTheHamster',
-    avatarPath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Harry_Treadaway_SDCC_2014_%28cropped%29.jpg/1200px-Harry_Treadaway_SDCC_2014_%28cropped%29.jpg',
-  }),
-  [sallyId]: new User({
-    id: sallyId,
-    email: 'sally@gmail.com',
-    name: 'Sallyorg',
-    avatarPath: 'https://vignette.wikia.nocookie.net/kpop/images/2/2e/Gugudan_Sally_Act_1_The_Little_Mermaid_photo_2.png/revision/latest?cb=20161025230221.jpg',
-  }),
-  [janeId]: getJane(),
-});
