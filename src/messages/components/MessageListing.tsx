93e7ad8cfa44890297784563e978a64b4ed1c621
LindaH32 <lindah@kentico.com>
2018-12-11 17:33:38 +0100

    Kill numbers in list

--- src/messages/components/MessageListing.tsx
+++ src/messages/components/MessageListing.tsx
@@ -21,7 +21,7 @@ export const MessageListing: React.SFC<IMessageListingDataProps> = ({ messages }
   };
 
   return (
-    <ol>
+    <ol className="none-list">
       {this._prepareMessages()}
     </ol>
   );
