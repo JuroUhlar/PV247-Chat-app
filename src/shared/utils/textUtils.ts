25f5b639dfa5ebce16aa26b58a14629a1cb3ae3e
LindaH32 <lindah@kentico.com>
2018-12-16 20:31:59 +0100

    Add tests for shared utils

--- src/shared/utils/textUtils.ts
+++ src/shared/utils/textUtils.ts
@@ -1,3 +1,1 @@
-export const isInsertEmpty = (text?: string): boolean => (!!text && text.trim() === '');
-
-export const addLineBreaks = (text: string): string => (text && text.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+export const isInsertEmpty = (text?: string): boolean => (!!text && text.trim() === '' || !text);
