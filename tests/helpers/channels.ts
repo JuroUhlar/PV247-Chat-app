c34c73df2bcde915a096236eee0efbcc31f8a19e
Juraj Uhlar <JurajU@kentico.com>
2019-01-05 17:19:11 +0100

    ChannelIds reducer tests

--- tests/helpers/channels.ts
+++ tests/helpers/channels.ts
@@ -2,16 +2,31 @@ import { Channel } from '../../src/channels/models/Channel';
 import { user1Id, user2Id } from './users';
 import * as Immutable from 'immutable';
 
+
+export const channelUsers = Immutable.List([user1Id, user2Id]);
+
 export const channel1 = new Channel({
   name: 'General',
   id: 'e569c598-91f7-47ef-a0ab-44bfb3635a12',
-  users: Immutable.List([user1Id, user2Id])
+  users: channelUsers
 });
 
 export const channel2 = new Channel({
   name: 'Random',
   id: 'b9b0e2b0-ed4c-4a3a-ac28-4e34cc2009a7',
-  users: Immutable.List([user1Id, user2Id])
+  users: channelUsers
+});
+
+export const channel3 = new Channel({
+  name: 'Dog videos',
+  id: '95832ad1-b950-4579-8a4d-c003467fcbee',
+  users: channelUsers
+});
+
+export const channel4 = new Channel({
+  name: 'Memes',
+  id: 'd4a2872c-1ebb-4ee4-aaf5-8db84d767c63',
+  users: channelUsers
 });
 
 export const channelNoUsers = new Channel({
