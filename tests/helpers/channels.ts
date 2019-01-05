7ee859d0f68a02301f11c9e8fe8b52d2ac68657e
Juraj Uhlar <JurajU@kentico.com>
2019-01-05 14:40:07 +0100

    selectedChannelReducer tests

--- tests/helpers/channels.ts
+++ tests/helpers/channels.ts
@@ -7,3 +7,15 @@ export const channel1 = new Channel({
   id: 'e569c598-91f7-47ef-a0ab-44bfb3635a12',
   users: Immutable.List([user1Id, user2Id])
 });
+
+export const channel2 = new Channel({
+  name: 'Random',
+  id: 'b9b0e2b0-ed4c-4a3a-ac28-4e34cc2009a7',
+  users: Immutable.List([user1Id, user2Id])
+});
+
+export const channelNoUsers = new Channel({
+  name: 'Work',
+  id: '80f5f2e6-cd0c-4f94-a9b3-066580ee5c9e',
+  users: Immutable.List([])
+});
