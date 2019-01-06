2bac178b396fb5a472410cf9c97b00a362e11433
Juraj Uhlar <JurajU@kentico.com>
2019-01-06 20:12:07 +0100

    Scroll message listing to the bottom automatically

--- src/messages/components/MessageListing.tsx
+++ src/messages/components/MessageListing.tsx
@@ -8,27 +8,57 @@ export interface IMessageListingDataProps {
   readonly messages: Immutable.OrderedMap<Uuid, IMessage>;
 }
 
-export const MessageListing: React.SFC<IMessageListingDataProps> = ({ messages }) => {
-  this._prepareMessages = () => {
+export class MessageListing extends React.PureComponent<IMessageListingDataProps>   {
+  static displayName = 'MessageListing';
+  static propTypes = {
+    messages: PropTypes.object,
+  };
+  private messagesBottomRef = React.createRef<HTMLDivElement>();
+
+  constructor(props: IMessageListingDataProps) {
+    super(props);
+  }
+
+
+  _prepareMessages = (messages: Immutable.OrderedMap<Uuid, IMessage>) => {
     return (messages && messages.map((message: IMessage, key: Uuid) => {
-        return (
-          <li key={key}>
-            <MessageContainer message={message}/>
-          </li>
-        );
-      })
+      return (
+        <li key={key}>
+          <MessageContainer message={message} />
+        </li>
+      );
+    })
     ).toArray();
   };
 
-  return (
+  _scrollToBottom = () => {
+    const node = this.messagesBottomRef.current;
+    if (node) {
+      node.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(this._scrollToBottom, 200);
+  }
+
+  componentDidUpdate(oldProps: IMessageListingDataProps) {
+    const oldFirstMessageId = oldProps.messages.keySeq().toArray()[0];
+    const newFirstMessageId = this.props.messages.keySeq().toArray()[0];
+    const oldCount = oldProps.messages.count();
+    const newCount = this.props.messages.count();
+    if (oldFirstMessageId === newFirstMessageId && newCount > oldCount) {
+      setTimeout(this._scrollToBottom, 200);
+    }
+  }
+
+  render = () => (
     <ol className="none-list">
-      {this._prepareMessages()}
+      {this._prepareMessages(this.props.messages)}
+      <div ref={this.messagesBottomRef} />
     </ol>
   );
-};
+
+}
 
 
-MessageListing.displayName = 'MessageListing';
-MessageListing.propTypes = {
-  messages: PropTypes.object,
-};
