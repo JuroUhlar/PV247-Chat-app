bfa9cdca18a2628962d3ce82c9e600b54b98f4eb
LindaH32 <lindah@kentico.com>
2019-01-06 17:09:57 +0100

    Mock css modules

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   testRegex: "^.+\\.test\\.(j|t)sx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  "moduleNameMapper": {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
